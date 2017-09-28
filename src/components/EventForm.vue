a2d2903fdb0cc741242ce7213fb9d79603ea3258
Kostya Danko <Danko.Kostiantyn@bcgdv.com>
2017-09-28 10:17:13 -0700

    added event form

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -1,15 +1,55 @@
 <template>
-    <div id="event-form" :class="{ active: true }" :style="{ top: top, left: left }"></div>
+    <div id="event-form" :class="{ active: active }" :style="{ top: top, left: left }">
+        <h4>Add an event</h4>
+        <p>{{ date.format('dddd, MMM Do') }}</p>
+        <div class="text">
+            <input v-focus type="text" v-model="description" placeholder="Event description" @keyup.enter="create">
+        </div>
+        <div class="buttons">
+            <button @click="create">Create</button>
+        </div>
+        <button id="close-button" @click="close">&#10005</button>
+    </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                description: ''
+            }
+        },
+        methods: {
+            create() {
+                if (this.description.length) {
+                    this.$store.commit('addEvent', this.description);
+                    this.close();
+                }
+            },
+            close() {
+                this.description = '';
+                this.$store.commit('eventFormActive', false);
+            }
+        },
         computed: {
+            date() {
+                return this.$store.state.eventFormDate;  
+            },
+            active() {
+                return this.$store.state.eventFormActive;
+            },
             top() {
                 return `${this.$store.state.eventFormPosY}px`;
             },
             left() {
                 return `${this.$store.state.eventFormPosX}px`;
+            }
+        },
+        directives: {
+            focus: {
+                update(el) {
+                    el.focus();
+                }
             }
         }
     }
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,24 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 Vue.use(Vuex);
 
+import moment from 'moment-timezone';
+moment.tz.setDefault('UTC');
+
+import Axios from 'axios';
+
 export default new Vuex.Store ({
     state: {
         currentYear: 2017,
         currentMonth: 9,
         eventFormPosX: 0,
-        eventFormPosY: 0
+        eventFormPosY: 0,
+        eventFormActive: false,
+        events: [
+              { description: 'Random event 1', date: moment('2017-09-11', 'YYYY-MM-DD') },
+              { description: 'Random event 2', date: moment('2017-09-13', 'YYYY-MM-DD') },
+              { description: 'Random event 3', date: moment() }
+        ],
+        eventFormDate: moment()
     },
     mutations: {
         setCurrentMonth(state, payload) {
@@ -19,6 +31,20 @@ export default new Vuex.Store ({
         eventFormPos(state, payload) {
             state.eventFormPosX = payload.x;
             state.eventFormPosY = payload.y;
+        },
+        eventFormActive(state, payload) {
+            state.eventFormActive = payload;
+        },
+        addEvent(state, payload) {
+            let event = {
+                description: payload,
+                date: state.eventFormDate
+            };
+            state.events.push(event);
+            Axios.post('/add_event', event);
+        },
+        eventFormDate(state, payload) {
+            state.eventFormDate = payload;
         }
     }
 });
